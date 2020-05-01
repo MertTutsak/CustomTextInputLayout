26e1d27ccbc5eab8dfe61138e519a7da3dc80cd3
Mert Tutsak <mert.tutsak@loodos.com>
2020-05-01 22:54:54 +0300

    color fixed

--- app/src/main/java/com/custom/textinputlayout/MainActivity.kt
+++ app/src/main/java/com/custom/textinputlayout/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.custom.textinputlayout.custom.component.INFO_STATE
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,12 +13,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        this.textInputLayout.setEndIconClickListener{
+        this.textInputLayout.setEndIconClickListener {
             this.textInputLayout.clear()
         }
 
+
         this.textInputLayout.onTextChanged {
             this.textInputLayout.setEndIconVisible(!it.isNullOrEmpty())
+
+            if (it.isNullOrEmpty()) {
+                this.textInputLayout.setInfoState(INFO_STATE.DEFAULT)
+            } else {
+                this.textInputLayout.setInfoState(INFO_STATE.ERROR)
+            }
         }
     }
 }
